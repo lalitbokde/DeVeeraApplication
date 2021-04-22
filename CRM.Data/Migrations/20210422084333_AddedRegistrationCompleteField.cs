e94aa41c9a4130136a368ecb34babe0de103d83a
Arpit-shivhare <shivharea532@gmail.com>
2021-04-22 14:23:33 +0530

    changes

---
+++ CRM.Data/Migrations/20210422084333_AddedRegistrationCompleteField.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRM.Data.Migrations
+{
+    public partial class AddedRegistrationCompleteField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "RegistrationComplete",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationComplete",
+                table: "User");
+        }
+    }
+}
