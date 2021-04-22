e2cfb9b07fb1ff7bb531638c074bbc08ec6c9360
rahulghagre <rahulghagre@users.noreply.github.com>
2021-04-22 14:23:39 +0530

    changes

---
+++ CRM.Core/Domain/VideoModules/Diary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.Core.Domain.VideoModules
+{
+    public class Diary : BaseEntity
+    {
+        public int LevelId { get; set; }
+        public string Comment { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool Deleted { get; set; }
+        public virtual Level Level { get; set; }
+    }
+}
---
+++ CRM.Data/Migrations/20210422084831_DiaryTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRM.Data.Migrations
+{
+    public partial class DiaryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diaries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LevelId = table.Column<int>(nullable: false),
+                    Comment = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diaries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Diaries_Levels_LevelId",
+                        column: x => x.LevelId,
+                        principalTable: "Levels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Diaries_LevelId",
+                table: "Diaries",
+                column: "LevelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Diaries");
+        }
+    }
+}
