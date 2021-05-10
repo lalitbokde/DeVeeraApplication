50b52c8e43429e175b1de468dc3b1c5bc9ee367c
rahulghagre <rahulghagre@users.noreply.github.com>
2021-05-10 21:03:11 +0530

    changes

---
+++ DeVeeraApp/ViewModels/QuestionAnswer/QuestionModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeVeeraApp.ViewModels.QuestionAnswer
+{
+    public class QuestionModel:BaseEntityModel
+    {
+        public QuestionModel()
+        {
+            AvailableLevels = new List<SelectListItem>();
+            AvailableModules = new List<SelectListItem>();
+        }
+        public int LevelId { get; set; }
+        public int ModuleId { get; set; }
+        public string Question { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool Deleted { get; set; }
+        public IList<SelectListItem> AvailableLevels { get; set; }
+        public IList<SelectListItem> AvailableModules { get; set; }
+    }
+}
