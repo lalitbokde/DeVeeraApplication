8edd749b409f5f21070c9905d345039b09cee6b8
Arpit-shivhare <shivharea532@gmail.com>
2021-05-10 13:55:23 +0530

    changes

---
+++ DeVeeraApp/ViewModels/LanguageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeVeeraApp.ViewModels
+{
+    public class LanguageModel : BaseEntityModel
+    {
+        public string LanguageName { get; set; }
+        public string Abbreviations { get; set; }
+
+    }
+}
