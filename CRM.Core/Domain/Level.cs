7e21ac8e20f9e5336c5943b4ed023bb61873d4b4
Arpit-shivhare <shivharea532@gmail.com>
2021-05-20 20:12:00 +0530

    changes

--- CRM.Core/Domain/Level.cs
+++ CRM.Core/Domain/Level.cs
@@ -13,6 +13,18 @@ namespace CRM.Core.Domain
         //public string Quote { get; set; }
         public string FullDescription { get; set; }
         public bool Active { get; set; }
+        public EmotionType Emotions { get; set; }
         public virtual Video Video { get; set; }
+    }
+
+
+    public enum EmotionType
+    {
+        Fear = 1,
+        Anger = 2,
+        Lust = 3,
+        Sadness = 4,
+        Ego = 5,
+        Happy = 6
     }
 }
