0cc75cd00e34843700737574fa9c43a99627f6e3
lalitbokde <lalitthinker@gmail.com>
2021-06-01 15:18:08 +0400

    changes

--- DeVeeraApp/Program.cs
+++ DeVeeraApp/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,20 +14,13 @@ namespace DeVeeraApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureKestrel(serverOptions =>
-                      {
-                          serverOptions.Limits.MaxRequestBodySize = null;
-                      });
-
-                    webBuilder.UseStartup<Startup>();
-
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseUrls("http://localhost:5000")
+            .UseKestrel()
+            .UseStartup<Startup>();
     }
 }
