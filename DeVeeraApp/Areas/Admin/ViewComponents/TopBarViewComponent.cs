c53df707907c3b4f9197e1415ce90948ffc97e75
Apurva Raut <37258406+apurvaraut@users.noreply.github.com>
2021-06-11 16:34:58 +0400

    changes for new admin Panel

---
+++ DeVeeraApp/Areas/Admin/ViewComponents/TopBarViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using CRM.Services;
+using DeVeeraApp.ViewModels;
+using DeVeeraApp.ViewModels.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeVeeraApp.Areas.Admin.ViewComponents
+{
+    public class TopBarViewComponent : ViewComponent
+    {
+       
+
+        public TopBarViewComponent()
+        {
+           
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+
+        
+            return View();
+        }
+
+    }
+}
