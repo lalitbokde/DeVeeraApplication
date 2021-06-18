7b0632133bb73f0aa078cdf3c4ed014bf1ff7e42
rahulghagre <rahulghagre@users.noreply.github.com>
2021-06-18 17:10:51 +0530

    changes

---
+++ DeVeeraApp/Areas/Admin/ViewComponents/PaginationViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using DeVeeraApp.Utils;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeVeeraApp.Areas.Admin.ViewComponents
+{
+    public class PaginationViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(PagedResultBase result)
+        {
+            return View("Default", result);
+        }
+    }
+}
