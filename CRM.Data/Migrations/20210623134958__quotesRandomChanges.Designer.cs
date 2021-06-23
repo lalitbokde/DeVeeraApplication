88239a32efe528a8a55764a31301ad060b4dea16
Apurva Raut <37258406+apurvaraut@users.noreply.github.com>
2021-06-23 17:58:02 +0400

    migrations

--- CRM.Data/Migrations/20210623134958__quotesRandomChanges.Designer.cs
+++ CRM.Data/Migrations/20210623134958__quotesRandomChanges.Designer.cs
@@ -4,14 +4,16 @@ using CRM.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CRM.Data.Migrations
 {
     [DbContext(typeof(dbContextCRM))]
-    partial class dbContextCRMModelSnapshot : ModelSnapshot
+    [Migration("20210623134958__quotesRandomChanges")]
+    partial class _quotesRandomChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
