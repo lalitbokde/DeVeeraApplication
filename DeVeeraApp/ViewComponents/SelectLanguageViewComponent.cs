be4ed05c9b38de623d433e00101466780c1629e2
Deepak Kumar <kumardeepak.pr@gmail.com>
2021-07-26 21:05:33 -0700

    Language change and opt verication page but problem in style of otp page

--- DeVeeraApp/ViewComponents/SelectLanguageViewComponent.cs
+++ DeVeeraApp/ViewComponents/SelectLanguageViewComponent.cs
@@ -1,17 +1,26 @@
-﻿using CRM.Services;
+﻿using CRM.Core;
+using CRM.Services;
+using CRM.Services.Settings;
 using DeVeeraApp.ViewModels.User;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
 
 namespace DeVeeraApp.ViewComponents
 {
     public class SelectLanguageViewComponent : ViewComponent
     {
         private readonly ILanguageService _languageService;
+        private readonly ISettingService _settingService;
+        private readonly IWorkContext _WorkContextService;
 
-        public SelectLanguageViewComponent(ILanguageService languageService)
+        public SelectLanguageViewComponent(ILanguageService languageService,
+                                           ISettingService settingService ,    
+                                           IWorkContext workContext)
         {
             _languageService = languageService;
+            _settingService = settingService;
+            _WorkContextService = workContext;
         }
 
 
@@ -20,15 +29,35 @@ namespace DeVeeraApp.ViewComponents
 
             model.LandingPageModel.Language.AvailableLanguages.Add(new SelectListItem { Text = "Select Language", Value = "0" });
             var AvailableLanguage = _languageService.GetAllLanguages();
+           
             foreach (var item in AvailableLanguage)
             {
+
                 model.LandingPageModel.Language.AvailableLanguages.Add(new SelectListItem
                 {
                     Value = item.Id.ToString(),
-                    Text = item.Name
-                });
+                    Text = item.Name,
+                    //Selected =(
+                    //Selected = item.Id == model.LandingPageModel.Language.Id
+                }) ;
             }
-
+            var userLanguage = _settingService.GetAllSetting().Where(s => s.UserId == _WorkContextService.CurrentUser?.Id).FirstOrDefault();
+            var guestLanguage = _settingService.GetSetting();
+            if (userLanguage!=null)
+            {
+                model.LandingPageModel.Language.Id = userLanguage.LanguageId;
+            }
+            else if (guestLanguage!=null)
+            {
+                model.LandingPageModel.Language.Id = guestLanguage.LanguageId;
+            }
+            else
+            {
+                var defaultLanguage = _languageService.GetAllLanguages().Where(s => s.Name == "English").FirstOrDefault().Id;
+                model.LandingPageModel.Language.Id = defaultLanguage;
+            }
+            
+            
             return View(model);
         }
 
