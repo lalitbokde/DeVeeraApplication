75922c5171958d40db909facaeb79be02ce27171
AshwiniDurug <ashidurugkar@gmail.com>
2021-09-20 14:52:49 +0530

    done

--- CRM.Services/TwilioConfiguration/Configuration.cs
+++ CRM.Services/TwilioConfiguration/Configuration.cs
@@ -7,7 +7,7 @@ namespace Configuration
 
         //old token
         //public string AuthToken { get; set; } = "ab95532d32935182d022b369521848d0";
-        public string AuthToken { get; set; } = "d7ade836b4afe5a70fed460ba5321c2f";
+        public string AuthToken { get; set; } = "2bcfb92abec88d6c5a043e8c5a13ae31";
        
         public string VerificationSid { get; set; } = "VA0dc2634edcf87d0df2ff0ae1352e9ef4";
     }
