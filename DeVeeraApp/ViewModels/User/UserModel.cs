307f9c0579fce6281ae6b0a0c32487afbad6f41b
Nancy30 <nancysharma370@gmail.com>
2021-11-29 13:10:09 +0530

    done

--- DeVeeraApp/ViewModels/User/UserModel.cs
+++ DeVeeraApp/ViewModels/User/UserModel.cs
@@ -1,4 +1,5 @@
 ﻿using CRM.Core.Domain.Users;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
@@ -82,6 +83,9 @@ namespace DeVeeraApp.ViewModels.User
         public string PasswordUpdate { get; set; }
 
 
+       
+        [Display(Name = "Profile Picture")]
+        public IFormFile ProfileImage { get; set; }
 
         [NotMapped]
         public bool TwoFactorAuthentication { get; set; }
