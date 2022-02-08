773dc44717dfdfa895df73609b2b6510689535fa
aakansha12 <aakanshadalvi@gmail.com>
2022-02-08 15:14:37 +0530

    migration added

---
+++ CRM.Data/Migrations/20220208094215_ProfileImageUrl.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRM.Data.Migrations
+{
+    public partial class ProfileImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageUrl",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Twilio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AccountSid = table.Column<string>(nullable: true),
+                    AuthToken = table.Column<string>(nullable: true),
+                    VerificationSid = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Twilio", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Twilio");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImageUrl",
+                table: "User");
+        }
+    }
+}
